526112aa0098e99ccef6eca6a40f1e070627563c
n0umen0n <46645013+n0umen0n@users.noreply.github.com>
2022-08-11 11:02:27 +0300

    wax chain config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Admin from './Admin';
-import reportWebVitals from './reportWebVitals';
-import { UALProvider, withUAL } from 'ual-reactjs-renderer'
-import { Anchor } from 'ual-anchor'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Admin from "./Admin";
+import reportWebVitals from "./reportWebVitals";
+import { UALProvider, withUAL } from "ual-reactjs-renderer";
+import { Anchor } from "ual-anchor";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const appName = "Fractal Voting";
+const appName = "Fractal Democracy";
 
 const chain = {
-  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+  chainId: "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4",
   rpcEndpoints: [
     {
       protocol: "https",
-      host: "eos.greymass.com",
+      host: "wax.greymass.com",
       port: "",
     },
   ],
@@ -30,24 +26,22 @@ const anchor = new Anchor([chain], {
 });
 
 const supportedChains = [chain];
-const supportedAuthenticators = [
-  anchor
-];
+const supportedAuthenticators = [anchor];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <UALProvider
-          chains={supportedChains}
-          authenticators={supportedAuthenticators}
-          appName={appName}
-        >
-          <Routes>
-            <Route path="/" element={<App />}/>
-            <Route path="/admin" element={<Admin />}/>
-          </Routes>
-        </UALProvider>
+      <UALProvider
+        chains={supportedChains}
+        authenticators={supportedAuthenticators}
+        appName={appName}
+      >
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </UALProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
