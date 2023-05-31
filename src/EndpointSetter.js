541c5a7eb998a3d26013c0bda4a8691ea5452201
lennarlehestik <lehestiklennar@gmail.com>
2023-05-31 12:19:26 +0300

    endpoint menu styling

--- src/EndpointSetter.js
+++ src/EndpointSetter.js
@@ -15,14 +15,24 @@ export default function EndpointSetter() {
     }
 
     return (
-      <Box sx={{ minWidth: 120 }}>
+      <Box sx={{ minWidth: 120, backgroundImage: "linear-gradient(144deg, #2f00ff, #5B42F3 50%, #00DDEB)", borderRadius:"10px", padding:"3px", marginTop:"1px"}}>
       <FormControl fullWidth>
             <Select
                 shrink={false}
                 defaultValue={localStorage.getItem("endpoint")}
                 labelId="demo-simple-select-label"
                 onChange={handleDropdownChange}
-                sx={{ backgroundColor:'white', color:"black" }}
+                inputProps={{
+                  MenuProps: {
+                      MenuListProps: {
+                          sx: {
+                            backgroundImage: "linear-gradient(-200deg, #010d1b, #061f31)",
+                            color:'rgba(255, 255, 255, 0.73)',
+                          }
+                      }
+                  }
+              }}
+                sx={{'& .MuiSvgIcon-root': {color: 'rgba(255, 255, 255, 0.73)'}, backgroundImage: "linear-gradient(-200deg, #010d1b, #061f31)", color:"rgba(255, 255, 255, 0.73)", borderRadius:"10px"}}
                 >
                 <MenuItem value="https://chain.wax.io">Chain Wax</MenuItem>
                 <MenuItem value="https://wax.cryptolions.io">Cryptolions</MenuItem>
